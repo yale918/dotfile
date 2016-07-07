d1c3bb1cdda0065691b54c03a642967a3a99d311
yale918 <yale918@gmail.com>
2016-07-07 14:14:53 +0800

    ..

---
+++ django/djangoSetting.sh
@@ -0,0 +1,8 @@
+sudo apt-get install python3-setuptools -y
+sudo easy_install3 pip
+sudo pip install virtualenv
+sudo vurtualenv envMachine
+sudo source ./envMachine/bin/activate
+sudo pip install "django<1.9"
+python3
+
